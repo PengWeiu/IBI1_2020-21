8fbc4c4cc626692ecda107ca41358f2417a24f12
PengWeiu <79951615+PengWeiu@users.noreply.github.com>
2021-03-17 09:33:38 +0800

    Delete Practical5 directory

--- Practical5/r_rate.py
+++
@@ -1,9 +0,0 @@
-#the infection is incremented with the rate of 1.2
-#five times means n*(1+r)**5
-n=84
-r=1.2
-for i in range(1,6):
- m=n*(1+r)**i
- i=i+1
-
-print "the r rate is ",str(r),"and the total number of individuals infected after 5 generations is",str(m)
