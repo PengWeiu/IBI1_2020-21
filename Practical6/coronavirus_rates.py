be7b788075424743f1b20ab6f8dc99bd534b802b
PengWeiu <79951615+PengWeiu@users.noreply.github.com>
2021-03-17 16:17:21 +0800

    Update coronavirus_rates.py

--- Practical6/coronavirus_rates.py
+++ Practical6/coronavirus_rates.py
@@ -4,7 +4,7 @@ print (coronavirus_infection)
 #add a plot
 import matplotlib.pyplot as plt
 labels='USA','India','Brazil','Russia','UK' #determine the labels
-sizes=[29872124,11285561,11205972,4360823,4234924] #determine the sizes
+sizes=[29862124,11285561,11205972,4360823,4234924] #determine the sizes
 explode=(0.1,0,0,0,0)
 #make the plot
 plt.pie(sizes,explode=explode,labels=labels,autopct="%1.1f%%",shadow=False,startangle=90)
