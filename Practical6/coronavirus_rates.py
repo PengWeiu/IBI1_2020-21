f0172f0ef57de94d59807cc96bdaeb4018688687
PengWeiu <79951615+PengWeiu@users.noreply.github.com>
2021-03-17 09:49:18 +0800

    Add files via upload

---
+++ Practical6/coronavirus_rates.py
@@ -0,0 +1,11 @@
+#input data to the dictionary
+coronavirus_infection={'USA':29862124,'India':11285561,'Brazil':11205972,'Russia':4360823,'UK':4234924}
+#add a plot
+import matplotlib.pyplot as plt
+labels='USA','India','Brazil','Russia','UK' #determine the labels
+sizes=[29872124,11285561,11205972,4360823,4234924] #determine the sizes
+explode=(0.1,0,0,0,0)
+#make the plot
+plt.pie(sizes,explode=explode,labels=labels,autopct="%1.1f%%",shadow=False,startangle=90)
+plt.axis('equal')
+plt.show()
---
+++ Practical6/list_manipulation.py
@@ -0,0 +1,19 @@
+#imput two list
+gene_lengths=[9410,394141,4442,105338,19149,76779,126550,36296,843,15981]
+exon_counts=[51,1142,42,216,25,650,32533,57,1,523]
+#define the list of averge exon length
+exon_lengths=[gene_lengths[0]/exon_counts[0]]
+#calculate and add them into the list
+for i in range (1,10):
+  exon_lengths1=[gene_lengths[i]/exon_counts[i]]
+  exon_lengths += exon_lengths1
+  i=i+1
+
+#add a plot
+import numpy as np
+import matplotlib.pyplot as plt
+#input the score
+score = exon_lengths
+#make a plot
+plt.boxplot(score, vert =True, whis = 1.5,patch_artist =True, meanline = True,showbox =True, showfliers =True, notch= True)
+plt.show()
