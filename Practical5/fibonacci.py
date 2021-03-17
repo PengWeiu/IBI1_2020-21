d1464f1a7279778363f8682541e406774948cdac
PengWeiu <79951615+PengWeiu@users.noreply.github.com>
2021-03-17 09:50:53 +0800

    Update fibonacci.py

--- Practical5/fibonacci.py
+++ Practical5/fibonacci.py
@@ -1,14 +1,15 @@
 #Python fibonacci,py
-#n3=n1+n2 n4=n2+n3 ... n13=n11+n12
-#n1=1 n2=1 let n1 and n2 be assigned
-#print n3 n4 ......n13
+#print n1 n2 first
 n1=1
 n2=1
 print (n1)
 print (n2)
+#n3=n1+n2 n4=n2+n3 ... n13=n11+n12
 for i in range(3,14):
+#n1=1 n2=1 let n1 and n2 be assigned
  n=n1+n2
  n1=n2
  n2=n
- print (n)
+#print n3 n4 ......n13
+print (n)
  i=i+1
