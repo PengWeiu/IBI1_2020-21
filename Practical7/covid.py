168353aedbe319baad911bf42716ad785a365b77
PengWeiu <79951615+PengWeiu@users.noreply.github.com>
2021-05-17 13:09:03 +0800

    Update covid.py

--- Practical7/covid.py
+++ Practical7/covid.py
@@ -2,7 +2,7 @@ import os
 import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np
-os.chdir("/Users/彭伟昱/PycharmProjects/pythonProject1/Practical7")
+os.chdir("/Users/彭伟昱/PycharmProjects/pythonProject1/Practical7") #open the file
 covid_data = pd.read_csv("full_data.csv")
 
 print(covid_data.iloc[0:11:2,:])
@@ -12,11 +12,12 @@ print (covid_data.loc[(covid_data.loc[:,"location"] == "Afghanistan"),"total_cas
 #print(covid_data.loc[(covid_data.loc[:,"location"] == "World"), "new_cases"].median())
 
 a=np.array(covid_data.loc[(covid_data.loc[:,"location"] == "World"),"new_cases"])
-b=np.mean(a)
-c=np.median(a)
+b=np.mean(a) #get the mean
+c=np.median(a) #get the median
 print (b)
 print (c)
 
+#creat a boxplot of new cases worldwide
 worldnewcases = covid_data.loc[(covid_data.loc[:,"location"] == "World"),"new_cases"]
 score1 = worldnewcases
 color = ['pink']
@@ -26,6 +27,7 @@ plt.ylabel("new cases")
 plt.boxplot(score1,notch= False, vert = True,whis=1.5, patch_artist = True, meanline = True, showbox = True, showcaps= True,showfliers= True)
 plt.show()
 
+#creat a plot of new cases and new deaths worldwide
 worldnewdeaths = covid_data.loc[(covid_data.loc[:,"location"] == "World"),"new_deaths"]
 plt.title("the plot of world new cases and world new deaths")
 plt.xlabel("world new cases")
