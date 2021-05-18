787ca3577656d12369b4498565ba1010e821a4fb
PengWeiu <79951615+PengWeiu@users.noreply.github.com>
2021-05-18 15:58:58 +0800

    Update grade_calculator.py

--- Practical10/grade_calculator.py
+++ Practical10/grade_calculator.py
@@ -1,10 +1,10 @@
 class Student():
- def __init__(self,name,code,poster,final):
+ def __init__(self,name,code,poster,final): #initialize the class
   self.name=name
   self.code=code
   self.poster=poster
   self.final=final
- def grade(self):
+ def grade(self):  #define a function to calculate and output the grade
   grades=self.poster*0.3+self.final*0.3+self.code*0.4
   print (self.name,' ','Grade:',grades)
 
