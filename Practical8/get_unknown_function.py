178b7c1e8ec8eeff26b7694d29dc835887fbff7c
PengWeiu <79951615+PengWeiu@users.noreply.github.com>
2021-05-18 15:50:00 +0800

    Update get_unknown_function.py

--- Practical8/get_unknown_function.py
+++ Practical8/get_unknown_function.py
@@ -1,13 +1,13 @@
 import os
 import re
 os.chdir("/Users/彭伟昱/PycharmProjects/pythonProject1")
-function=open('Saccharomyces_cerevisiae.R64-1-1.cdna.all.fa','r')
+function=open('Saccharomyces_cerevisiae.R64-1-1.cdna.all.fa','r') #open and read the file
 lines=function.readlines()
 unknown_function=[]
 
 for i in range(len(lines)):
   if re.findall('unknown function',lines[i]):
-    name=re.search(r'(>.+?)(?:_| )',lines[i])
+    name=re.search(r'(>.+?)(?:_| )',lines[i])#find and add the name of unknown function DNA in the list
     name=name.group()
     name=name.replace('_','')
     unknown_function.append(name)
@@ -19,7 +19,7 @@ for i in range(len(lines)):
         if lines[i+x+1].startswith('>'):
             break
         else:
-            preseq+=lines[i+x+1]
+            preseq+=lines[i+x+1]#find and add the sequence of unknown function DNA in the list
     sequence+=preseq.replace('\n','')
     unknown_function.append(sequence)
 
@@ -30,13 +30,13 @@ dictionary={}
 for i in range(len(unknown_function)):
      m=unknown_function[i]
      if m.startswith('>'):
-        length=str(len(unknown_function[i+1])-1)
+        length=str(len(unknown_function[i+1])-1)#calculate and add the length of unknown function DNA in the list
         unknown_function[i]+='\nLength:'
         unknown_function[i]+=length
         unknown_function[i+2]+='.\n'
 
 unknown=open('unknown_function.fa','w')
-for i in unknown_function:
+for i in unknown_function:  #add the context of the list'unknown_function' into the file
  n=str(i)
  m=str(n)
  unknown.write(m)
