dc5e24c4d845bd6eee825606b58bfe0ff6b7467f
PengWeiu <79951615+PengWeiu@users.noreply.github.com>
2021-05-18 16:12:42 +0800

    Update student_list.py

--- Practical10/student_list.py
+++ Practical10/student_list.py
@@ -1,10 +1,12 @@
 class Student():
- def __init__(self,first_name,last_name,undergraduate_programme): #initialize the class
+#initialize the class
+ def __init__(self,first_name,last_name,undergraduate_programme): 
   self.first_name=first_name
   self.last_name=last_name
   self.undergraduate_programme=undergraduate_programme
 
- def stu(self): #define a function to output student list
+#define a function to output student list
+ def stu(self): 
   return print ('full name:',self.first_name,'',self.last_name,'    ','programe:',self.undergraduate_programme)
 
 #example
